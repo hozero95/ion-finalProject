ee3f2b78fa74a6a3a84b4257d9ea0c9f306ff197
sky4564 <dobe4564@naver.com>
2021-11-02 18:30:32 +0900

    frontend init

---
+++ finalProject/src/frontend/vue_page/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <button v-on:click="fetchData">get data</button>
+    
+  
+
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  
+  methods: {
+    fetchData: function() {
+      axios.get('http://localhost:8000/api/main/show/nav')
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ finalProject/src/frontend/vue_page/src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <p>here is test page 입니다.</p>
+    <p>here is test page 입니다.</p>
+    <p>아무거나 적어야지</p>
+  </div>
+  
+  
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    data(){
+        return{
+            msg: 'test page'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
