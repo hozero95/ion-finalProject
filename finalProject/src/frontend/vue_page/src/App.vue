512c942813b9f2abeb989e6c959317c4b8db202e
hozero95 <the.hozero@gmail.com>
2021-11-07 18:26:57 +0900

    category mapper 작업

--- finalProject/src/frontend/vue_page/src/App.vue
+++ finalProject/src/frontend/vue_page/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <Header></Header>
-    <Navbar_top v-if="$store.state.viewFlag[1] == 1"></Navbar_top>
+    <!-- <NavbarTop v-if="$store.state.viewFlag[1] == 1"></NavbarTop> -->
+    <NavbarTop></NavbarTop>
     <router-view/>
-    <Footer v-if="$store.state.viewFlag[2] == 1"></Footer>
+    <!-- <Footer v-if="$store.state.viewFlag[2] == 1"></Footer> -->
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
-import Navbar_top from './components/Navbar-top.vue'
+import NavbarTop from './components/Navbar-top.vue'
 export default {
   name: 'App',
   data() {
@@ -19,7 +21,9 @@ export default {
     }
   },
   components : {
-    Header, Footer, Navbar_top
+    Header,
+    Footer,
+    NavbarTop
   }
   
 }
--- finalProject/src/frontend/vue_page/src/components/Navbar-top.vue
+++ finalProject/src/frontend/vue_page/src/components/Navbar-top.vue
@@ -1,112 +1,151 @@
 <template>
-    <!-- top navbar -->
-    <div class="navbar-top">
-        <ul class="navbar-top__menu">
-                <li class="navbar-top__menu__iteam">
-                    <a href="http://localhost:8080/#/category">생선</a> 
-                    <div class="dropdown-content">
-                        <router-link to="category"><a href="#">생물 생선</a></router-link>
-                        <router-link to="category"><a href="#">냉동 생선</a></router-link>
-                        <router-link to="category"><a href="#">반건조 생선</a></router-link>
-                        <router-link to="category"><a href="#">굴비</a></router-link>
-                    </div>
-			    </li>
-            
-                <li class="navbar-top__menu__iteam">
-                    <a href="http://localhost:8080/#/category">킹크랩, 꽃게, 랍스터</a>
-                    <div class="dropdown-content">
-                        <router-link to="category"><a href="#">킹크랩</a></router-link>
-                        <router-link to="category"><a href="#">꽃게</a></router-link>
-                        <router-link to="category"><a href="#">랍스터</a></router-link>
-                    </div>
-                </li>
+  <!-- top navbar -->
+  <div class="navbar-top" v->
+    <ul class="navbar-top__menu">
+      <li class="navbar-top__menu__iteam">
+        <a href="http://localhost:8001/#/category">생선</a>
+        <div class="dropdown-content">
+          <router-link to="category"><a href="#">생물 생선</a></router-link>
+          <router-link to="category"><a href="#">냉동 생선</a></router-link>
+          <router-link to="category"><a href="#">반건조 생선</a></router-link>
+          <router-link to="category"><a href="#">굴비</a></router-link>
+        </div>
+      </li>
 
-                <li class="navbar-top__menu__iteam">
-                    <a href="http://localhost:8080/#/category">새우류</a>
-                    <div class="dropdown-content">
-                        <router-link to="category"><a href="#">냉동 새우</a></router-link>
-                        <router-link to="category"><a href="#">횟감용 새우</a></router-link>
-                        <router-link to="category"><a href="#">활새우</a></router-link>
-                    </div>
-                </li>
-            <li class="navbar-top__menu__iteam">
-                <a href="http://localhost:8080/#/category">쭈꾸미, 오징어, 문어</a>
-				<div class="dropdown-content">
-					<router-link to="category"><a href="#">쭈꾸미, 낙지, 꼴뚜기</a></router-link>
-					<router-link to="category"><a href="#">오징어 한치</a></router-link>
-					<router-link to="category"><a href="#">문어</a></router-link>
-				</div>
-			</li>
-            <li class="navbar-top__menu__iteam">/</li>
-            <router-link to="seasonalProduct"><li class="navbar-top__menu__iteam">제철</li></router-link>
-            <router-link to="bestProduct"><li class="navbar-top__menu__iteam">BEST</li></router-link>
-            <router-link to="event"><li class="navbar-top__menu__iteam">이벤트</li></router-link>
-        </ul>
-    </div>
+      <li class="navbar-top__menu__iteam">
+        <a href="http://localhost:8001/#/category">킹크랩, 꽃게, 랍스터</a>
+        <div class="dropdown-content">
+          <router-link to="category"><a href="#">킹크랩</a></router-link>
+          <router-link to="category"><a href="#">꽃게</a></router-link>
+          <router-link to="category"><a href="#">랍스터</a></router-link>
+        </div>
+      </li>
+
+      <li class="navbar-top__menu__iteam">
+        <a href="http://localhost:8001/#/category">새우류</a>
+        <div class="dropdown-content">
+          <router-link to="category"><a href="#">냉동 새우</a></router-link>
+          <router-link to="category"><a href="#">횟감용 새우</a></router-link>
+          <router-link to="category"><a href="#">활새우</a></router-link>
+        </div>
+      </li>
+      <li class="navbar-top__menu__iteam">
+        <a href="http://localhost:8001/#/category">쭈꾸미, 오징어, 문어</a>
+        <div class="dropdown-content">
+          <router-link to="category"><a href="#">쭈꾸미, 낙지, 꼴뚜기</a></router-link>
+          <router-link to="category"><a href="#">오징어 한치</a></router-link>
+          <router-link to="category"><a href="#">문어</a></router-link>
+        </div>
+      </li>
+      <li class="navbar-top__menu__iteam">/</li>
+      <router-link to="seasonalProduct">
+        <li class="navbar-top__menu__iteam">제철</li>
+      </router-link>
+      <router-link to="bestProduct">
+        <li class="navbar-top__menu__iteam">BEST</li>
+      </router-link>
+      <router-link to="event">
+        <li class="navbar-top__menu__iteam">이벤트</li>
+      </router-link>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+  import axios from 'axios'
+  export default {
+    created() {
+      axios.get('http://localhost:8000/api/main/show/nav')
+        .then(res => {
+          var bigCate = new Array();
 
-}
+          var count = 0
+          for (var i = 0; i < res.data.length; i++) {
+            if (res.data[i].categoryRef == null || res.data[i].categoryRef == '') {
+              bigCate.push(res.data[i]);
+            }
+          }
+
+          var smallCate = new Array(bigCate.length);
+          for(var i = 0; i < smallCate.length; i++) {
+            smallCate[i] = new Array();
+          }
+
+          for (var i = 0; i < res.data.length; i++) {
+            if (!res.data[i].categoryRef == null || !res.data[i].categoryRef == '') {
+              for(var j = 0; j < bigCate.length; j++) {
+                if(res.data[i].categoryRef == bigCate[j].categoryUnum) {
+                  smallCate[j].push(res.data[i]);
+                }
+              }
+            }
+          }
+        });
+    }
+  }
 </script>
 
 <style>
-/* Universal tags */
-* {
+  /* Universal tags */
+  * {
     box-sizing: border-box;
-}
+  }
 
-body {
+  body {
     font-family: 'Open Sans', sans-serif;
     margin: 0;
-}
+  }
 
-a {
+  a {
     text-decoration: none;
     color: var(--color-white);
-}
+  }
 
-ul {
+  ul {
     padding-left: 0;
-}
+  }
 
-li {
+  li {
     list-style: none;
-}
+  }
 
-button {
+  button {
     background-color: transparent;
     cursor: pointer;
     border: none;
     outline: none;
     padding: 0;
-}
-p{
+  }
+
+  p {
     margin: 0;
-}
-.text1{
+  }
+
+  .text1 {
     margin: 0;
     font-size: 12px;
     color: gray;
-}
-.text2{
+  }
+
+  .text2 {
     margin: 0;
     font-size: 12px;
-}
-.text3{
+  }
+
+  .text3 {
     margin: 0;
     font-size: 11px;
     color: gray;
-}
+  }
 
-/* navber top */
-.navbar-top{
+  /* navber top */
+  .navbar-top {
     margin: 0;
     background-color: #132c7d;
     height: 60px;
-}
-.navbar-top__menu{
+  }
+
+  .navbar-top__menu {
     margin-left: 16%;
     width: 66%;
     height: 60px;
@@ -114,31 +153,35 @@ p{
     display: flex;
     justify-content: space-between;
     font-size: 20px;
-}
-.navbar-top__menu__iteam{
+  }
+
+  .navbar-top__menu__iteam {
     justify-content: center;
     margin-left: 30px;
     margin-top: 15px;
-    cursor:pointer;
-}
+    cursor: pointer;
+  }
 
-/* navbar dropdown */
-.dropdown-content {
+  /* navbar dropdown */
+  .dropdown-content {
     display: none;
     position: absolute;
     background-color: #f1f1f1;
     min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
     z-index: 1;
     margin-top: 15px;
-}
-.dropdown-content a {
+  }
+
+  .dropdown-content a {
     color: black;
     padding: 12px 0px;
     text-decoration: none;
     display: block;
-}
-.navbar-top__menu__iteam:hover .dropdown-content{
+  }
+
+  .navbar-top__menu__iteam:hover .dropdown-content {
     display: block;
-}
+  }
+
 </style>
