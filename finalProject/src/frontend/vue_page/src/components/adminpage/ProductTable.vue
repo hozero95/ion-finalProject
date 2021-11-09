cebb333b4d353ee8d7bcdfd7bb87be6fec9ce9f2
hozero95 <the.hozero@gmail.com>
2021-11-09 16:00:18 +0900

    관리자페이지 - 상품등록 완료

--- finalProject/src/frontend/vue_page/src/components/adminpage/ProductTable.vue
+++ finalProject/src/frontend/vue_page/src/components/adminpage/ProductTable.vue
@@ -1,4 +1,5 @@
 <template>
+
   <body>
     <div>{{ products[0] }}</div>
 
@@ -9,24 +10,22 @@
         <table class="table table-bordered" style="border: 2px solid black">
           <thead>
             <tr>
-              <th scope="col5">list</th>
-              <th scope="col0">상품</th>
-              <th scope="col1">카테고리</th>
-              <th scope="col2">상품이름</th>
-              <th scope="col3">상품가격</th>
-              <th scope="col4">등록일자</th>
-              <th scope="col5">시즌상품</th>
+              <th scope="col0">카테고리</th>
+              <th scope="col1">상품이름</th>
+              <th scope="col2">상품가격</th>
+              <th scope="col3">시즌</th>
+              <th scope="col4">이미지 1</th>
+              <th scope="col5">이미지 2</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th scope="row"></th>
-              <td><input type="text" /></td>
-              <td><input type="text" /></td>
-              <td><input type="text" /></td>
-              <td><input type="text" /></td>
-              <td><input type="text" /></td>
-              <td><input type="text" /></td>
+              <td><input v-model="addCategoryUnum" type="text" /></td>
+              <td><input v-model="addProductName" type="text" /></td>
+              <td><input v-model="addProductPrice" type="text" /></td>
+              <td><input v-model="addProductSeason" type="text" /></td>
+              <td><input v-model="addProductImage01" type="text"><br><input type="file" /></td>
+              <td><input v-model="addProductImage02" type="text"><br><input type="file" /></td>
             </tr>
           </tbody>
         </table>
@@ -47,18 +46,16 @@
         <table class="table table-bordered" style="border: 2px solid black">
           <thead>
             <tr>
-              <th scope="col5">list</th>
-              <th scope="col0">상품</th>
+              <th scope="col0">상품번호</th>
               <th scope="col1">카테고리</th>
               <th scope="col2">상품이름</th>
               <th scope="col3">상품가격</th>
               <th scope="col4">등록일자</th>
-              <th scope="col5">시즌상품</th>
+              <th scope="col5">시즌</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th scope="row"></th>
               <td>
                 <textarea v-model="products[ItemNum].productUnum"></textarea>
               </td>
@@ -101,12 +98,12 @@
               </span>
             </div>
           </th>
-          <th scope="col0">상품</th>
+          <th scope="col0">상품번호</th>
           <th scope="col1">카테고리</th>
           <th scope="col2">상품이름</th>
           <th scope="col3">상품가격</th>
           <th scope="col4">등록일자</th>
-          <th scope="col5">시즌상품</th>
+          <th scope="col5">시즌</th>
         </tr>
       </thead>
       <tbody>
@@ -130,92 +127,109 @@
 </template>
 
 <script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-      products: [],
-      AddModal: true,
-      MdfModal: false,
-      ItemNum: 0,
-    };
-  },
+  import axios from "axios";
+  export default {
+    data() {
+      return {
+        products: [],
+        AddModal: true,
+        MdfModal: false,
+        ItemNum: 0,
 
-  methods: {
-    AddItem() {
-      this.AddModal = true;
+        // add modal
+        addCategoryUnum: null,
+        addProductName: null,
+        addProductPrice: null,
+        addProductSeason: null,
+        addProductImage01: null,
+        addProductImage02: null
+      };
     },
-    ModifiedModal(index) {
-      this.MdfModal = true;
-      this.ItemNum = index;
-    },
+    methods: {
+      AddItem() {
+        this.AddModal = true;
+      },
+      ModifiedModal(index) {
+        this.MdfModal = true;
+        this.ItemNum = index;
+      },
 
-    ProductAdd() {
+      ProductAdd() {
+        var headers = {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + this.$store.state.jwtToken
+            };
+
+        var body = {
+          categoryUnum: this.addCategoryUnum,
+          productName: this.addProductName,
+          productPrice: this.addProductPrice,
+          productSeason: this.addProductSeason,
+          productImage01Path: this.addProductImage01,
+          productImage02Path: this.addProductImage02
+        };
+
+        axios.defaults.headers.post = null
+        axios({
+            url: 'http://localhost:8000/api/admin/product/add',
+            method: 'post',
+            headers: headers,
+            data: body
+          })
+          .then((res) => {
+            alert("상품 등록 " + res.data);
+            this.AddModal = false;
+          })
+          .catch((err) => {
+            alert("이미 있는 상품이거나, 등록할 수 없는 상품입니다.");
+            this.AddModal = false;
+          });
+      },
+
+    },
+    created() {
       var headers = {
         Authorization: "Bearer " + this.$store.state.jwtToken,
       };
-      var body = {
-        categoryUnum: 11,
-        productName: "asd",
-        productPrice: 11,
-        productRegdate: [1,2,3],
-        productSeason: 11,
-        productImage01Path: "asd",
-        productImage02Path: "asd",
-      };
-
       axios
-        .post("http://localhost:8000/api/admin/product/add", {
-          headers,body,
+        .get("http://localhost:8000/api/admin/product/all", {
+          headers,
         })
         .then((res) => {
-          console.log("hi dude");
-        })
-        .catch((err) => {
-          console.log(err);
+          var product = new Array();
+
+          for (var i = 0; i < res.data.length; i++) {
+            product.push(res.data[i]);
+          }
+          this.products = product;
         });
     },
+  };
 
-  },
-  created() {
-    var headers = {
-      Authorization: "Bearer " + this.$store.state.jwtToken,
-    };
-    axios
-      .get("http://localhost:8000/api/admin/product/all", {
-        headers,
-      })
-      .then((res) => {
-        var product = new Array();
-
-        for (var i = 0; i < res.data.length; i++) {
-          product.push(res.data[i]);
-        }
-        this.products = product;
-        console.log(this.products);
-      });
-  },
-};
 </script>
 
-<style>
-body {
-  margin: 30px;
-}
-div {
-  box-sizing: border-box;
-}
-.black-bg {
-  width: 100%;
-  height: 100%;
-  /* background: rgba(0,0,0,0.5); */
+<style scoped>
+  body {
+    margin: 30px;
+  }
 
-  position: fixed;
-}
-.white-bg {
-  width: fit-content;
-  background: white;
-  border-radius: 8px;
-  padding: 20px;
-}
+  div {
+    box-sizing: border-box;
+  }
+
+  .black-bg {
+    width: 100%;
+    height: 100%;
+    /* background: rgba(0,0,0,0.5); */
+
+    position: fixed;
+  }
+
+  .white-bg {
+    width: fit-content;
+    background: white;
+    border-radius: 8px;
+    padding: 20px;
+  }
+
 </style>
