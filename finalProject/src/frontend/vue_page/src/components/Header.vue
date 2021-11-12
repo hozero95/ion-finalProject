a7fb9d449f477dacb4da1cc43e2293083e7ed64b
sky4564 <dobe4564@naver.com>
2021-11-12 20:57:56 +0900

    add headericon animation , sidebar

--- finalProject/src/frontend/vue_page/src/components/Header.vue
+++ finalProject/src/frontend/vue_page/src/components/Header.vue
@@ -1,15 +1,21 @@
 <template>
-
   <body>
     <!-- header -->
     <header class="header">
       <!-- header left -->
-      <div class="header__logo">
-        <router-link to="/">
-          <img src="https://raw.githubusercontent.com/sky4564/img/master/FinalProjectImg/logoimg_white_backgound.jpg"
-            @click="showing" alt="">
-        </router-link>
-      </div>
+      
+        <div class="flip-outer">
+          <div class="flip-inner">
+            <router-link to="/">
+              <img
+                src="https://raw.githubusercontent.com/sky4564/img/master/FinalProjectImg/logoimg_white_backgound.jpg"
+                class="front"
+              />
+            </router-link>
+            <div class="back"></div>
+          </div>
+        </div>
+      
 
       <!-- 검색창 -->
       <!-- <div class="header__search">
@@ -23,13 +29,28 @@
       <ul class="header__menu">
         <button @click="checkFlag">viewflag 체크</button>
         <button @click="checkStore">store 체크</button>
-        <router-link to="login" v-if="this.$store.state.jwtToken == null || this.$store.state.jwtToken == ''">
+        <router-link
+          to="login"
+          v-if="
+            this.$store.state.jwtToken == null ||
+            this.$store.state.jwtToken == ''
+          "
+        >
           <li class="header__menu__iteam">로그인</li>
         </router-link>
-        <router-link to="login" v-if="this.$store.state.jwtToken != null && this.$store.state.jwtToken != ''">
+        <router-link
+          to="login"
+          v-if="
+            this.$store.state.jwtToken != null &&
+            this.$store.state.jwtToken != ''
+          "
+        >
           <li class="header__menu__iteam" @click="logout">로그아웃</li>
         </router-link>
-        <router-link to="AdminPage" v-if="this.$store.state.userInfo.authorities.length >= 2">
+        <router-link
+          to="AdminPage"
+          v-if="this.$store.state.userInfo.authorities.length >= 2"
+        >
           <li class="header__menu__iteam" @click="hiding">관리자 페이지</li>
         </router-link>
 
@@ -40,7 +61,9 @@
           <li class="header__menu__iteam"><i class="fas fa-truck"></i></li>
         </router-link>
         <router-link to="cart">
-          <li class="header__menu__iteam"><i class="fas fa-shopping-cart"></i></li>
+          <li class="header__menu__iteam">
+            <i class="fas fa-shopping-cart"></i>
+          </li>
         </router-link>
       </ul>
     </header>
@@ -48,179 +71,204 @@
 </template>
 
 <script>
-  export default {
-    methods: {
-      logout() {
-        this.$store.commit('logout');
-        alert('로그아웃 되었습니다.');
-      },
-      hiding() {
-        this.$store.commit('hideAll');
-      },
-      showing() {
-        this.$store.commit('showAll');
-      },
-      checkStore() {
-        console.log(this.$store.state.jwtToken);
-        console.log(this.$store.state.userInfo);
-      },
-      checkFlag() {
-        console.log(this.$store.state.viewflag);
-      }
-    }
-  }
+export default {
+  methods: {
+    logout() {
+      this.$store.commit("logout");
+      alert("로그아웃 되었습니다.");
+    },
+    hiding() {
+      this.$store.commit("hideAll");
+    },
+    showing() {
+      this.$store.commit("showAll");
+    },
+    checkStore() {
+      console.log(this.$store.state.jwtToken);
+      console.log(this.$store.state.userInfo);
+    },
+    checkFlag() {
+      console.log(this.$store.state.viewflag);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  /* Universal tags */
-  * {
-    box-sizing: border-box;
-  }
+.flip-outer {
+  width: 100px;
+  height: 100px;
+  
+}
 
-  body {
-    font-family: 'Open Sans', sans-serif;
-    margin: 0;
-  }
+.flip-inner {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
 
-  a {
-    text-decoration: none;
-    color: var(--color-white);
-  }
+.front {
+  width: 100%;
+  position: absolute;
+}
 
-  ul {
-    padding-left: 0;
-  }
+.back {
+  width: 100%;
+  position: absolute;
+  transform: rotateY(180deg);
+}
 
-  li {
-    list-style: none;
-  }
+.flip-inner {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transition: all 1s;
+  transform-style: preserve-3d;
+}
 
-  button {
-    background-color: transparent;
-    cursor: pointer;
-    border: none;
-    outline: none;
-    padding: 0;
-  }
+.flip-inner:hover {
+  transform: rotateY(180deg);
+}
 
-  p {
-    margin: 0;
-  }
+/* Universal tags */
+* {
+  box-sizing: border-box;
+}
 
-  .text1 {
-    margin: 0;
-    font-size: 12px;
-    color: gray;
-  }
+body {
+  font-family: "Open Sans", sans-serif;
+  margin: 0;
+}
 
-  .text2 {
-    margin: 0;
-    font-size: 12px;
-  }
+a {
+  text-decoration: none;
+  color: var(--color-white);
+}
 
-  .text3 {
-    margin: 0;
-    font-size: 11px;
-    color: gray;
-  }
+ul {
+  padding-left: 0;
+}
 
-  /* header */
+li {
+  list-style: none;
+}
+
+button {
+  background-color: transparent;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  padding: 0;
+}
+
+p {
+  margin: 0;
+}
+
+.text1 {
+  margin: 0;
+  font-size: 12px;
+  color: gray;
+}
+
+.text2 {
+  margin: 0;
+  font-size: 12px;
+}
+
+.text3 {
+  margin: 0;
+  font-size: 11px;
+  color: gray;
+}
+
+/* header */
+.header {
+  display: flex;
+  align-items: center;
+  padding: 0 300px;
+}
+
+.header__search {
+  width: 230px;
+  height: 15px;
+  margin-left: 30px;
+  /* margin-right: 35%; */
+}
+
+.input__header__search__input button {
+  /* position: absolute; */
+  /* left: 620px; */
+  /* top: 57px; */
+  margin-left: 5px;
+  color: #5172de;
+}
+
+.header__menu {
+  display: flex;
+  margin-top: 30px;
+}
+
+.header__menu__iteam {
+  padding: 8px 12px;
+  margin: 0 4px;
+}
+
+/* footer */
+
+.footer {
+  width: 100%;
+  height: 335px;
+  padding: 0 320px;
+}
+
+.footer__left {
+  width: 70%;
+  float: left;
+}
+
+.footer__left__row2 {
+  margin-top: 20px;
+}
+
+.footer__left__row2 img {
+  width: 750px;
+  height: 60px;
+  margin-left: 0;
+}
+
+.footer__left__row3 {
+  margin-top: 20px;
+}
+
+.footer__right {
+  width: 30%;
+  float: right;
+  padding-left: 180px;
+}
+
+.footer__right img {
+  width: 200px;
+  height: 80px;
+}
+
+/* 노트북 사이즈 */
+@media screen and (max-width: 1500px) {
   .header {
-    display: flex;
-    /* justify-content: space-around; */
-    align-items: center;
-    padding: 0 300px;
-  }
-
-  .header__logo img {
-    width: 100px;
-    height: 100px;
-    margin-top: 20px;
-  }
-
-  .header__search {
-    width: 230px;
-    height: 15px;
-    margin-left: 30px;
-    /* margin-right: 35%; */
-
+    padding: 0 80px;
   }
 
   .input__header__search__input button {
-    /* position: absolute; */
-    /* left: 620px; */
-    /* top: 57px; */
-    margin-left: 5px;
-    color: #5172de;
+    left: 387px;
+    top: 57px;
   }
-
-  .header__menu {
-    display: flex;
-    margin-top: 30px;
-  }
-
-  .header__menu__iteam {
-    padding: 8px 12px;
-    margin: 0 4px;
-
-  }
-
-  /* footer */
 
   .footer {
-    width: 100%;
-    height: 335px;
-    padding: 0 320px;
-  }
-
-  .footer__left {
-    width: 70%;
-    float: left;
-  }
-
-  .footer__left__row2 {
-    margin-top: 20px;
-  }
-
-  .footer__left__row2 img {
-    width: 750px;
-    height: 60px;
-    margin-left: 0;
-
-  }
-
-  .footer__left__row3 {
-    margin-top: 20px;
+    padding: 0 90px;
   }
 
   .footer__right {
-    width: 30%;
-    float: right;
-    padding-left: 180px;
+    padding-left: 170px;
   }
-
-  .footer__right img {
-    width: 200px;
-    height: 80px;
-  }
-
-  /* 노트북 사이즈 */
-  @media screen and (max-width: 1500px) {
-    .header {
-      padding: 0 80px;
-    }
-
-    .input__header__search__input button {
-      left: 387px;
-      top: 57px;
-    }
-
-    .footer {
-      padding: 0 90px;
-    }
-
-    .footer__right {
-      padding-left: 170px;
-    }
-  }
+}
 </style>
--- finalProject/src/frontend/vue_page/src/components/Review.vue
+++ finalProject/src/frontend/vue_page/src/components/Review.vue
@@ -1,293 +1,349 @@
 <template>
-  <body>
+  <body class="body_review">
     <div class="container_my">
+      <div class="header_my">
+        <p class="user_name">#손님111</p>
+        <img class="ssg_img" src="" alt="img" />
+      </div>
 
-        <div class="header_my">
-
-
-            <p class="user_name">#손님</p>
-            <img class="ssg_img" src="" alt="img">
-
-
-        </div>
-
-        <div class="content_my">
-
-
-
-
-            <div class="content_side">
-                <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown">
-
-                    <div class="btn-group " role="group">
-                        <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle btn_size"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            주문관리
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                            <li><a class="dropdown-item" href="#">주문/배송관리</a></li>
-                            <li><a class="dropdown-item" href="#"></a></li>
-                        </ul>
-                    </div>
-
-                    <div class="btn-group btn_size" role="group">
-                        <button id="btnGroupDrop1 " type="button" class="btn btn-primary dropdown-toggle btn_size"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            활동관리
-                        </button>
-                        <ul class="dropdown-menu " aria-labelledby="btnGroupDrop1">
-                            <li><a class="dropdown-item " href="#">리뷰관리</a></li>
-                            <li><a class="dropdown-item" href="#">Q&A관리</a></li>
-                        </ul>
-
-                    </div>
-
-                    <div class="btn-group" role="group">
-
-                        <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle btn_size"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            정보관리
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                            <li><a class="dropdown-item" href="#">회원정보 변경</a></li>
-                            <li><a class="dropdown-item" href="#">비밀번호 변경</a></li>
-                        </ul>
-                    </div>
-
-                </div>
+      <div class="content_my">
+        <div class="content_side">
+          <div class="card">
+            <div class="card-header hd1"  @click="ONOFF1">
+              주문관리
+              <button v-if="item1bt==0" style="margin-left :85px">
+                <i class="fas fa-chevron-up"></i>
+              </button>
+              <button v-if="item1bt==1" style="margin-left :85px">
+                <i class="fas fa-chevron-down"></i>
+              </button>
             </div>
 
-            <div class="content_body">
-
-                <h2 class="body_header">리뷰 관리</h2>
-                <img src="" alt="img" style="width: 777px; height: 62px;">
-
-                <div class="body_content">
-                    <h3>미등록리뷰 n건</h3>
-
-                    <div class="content_tb">
-                        <table>
-                            <colgroup>
-                                <col style="width:14%">
-                                <col>
-                                <col style="width:20%">
-                                <col style="width:17%">
-                            </colgroup>
-                            <thead>
-
-                                <tr class="th_box">
-                                    <th scope="col" style="">주문번호</th>
-                                    <th scope="col" style="">주문상품</th>
-                                    <th scope="col" style=""><span class="blind">리뷰 혜택</span></th>
-                                    <th scope="col" style=""><span class="blind">리뷰 쓰기</span></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="td_box">
-                                        <p style=""><span style="">해당 기간에 구매내역이 없습니다.</span></p>
-                                    </td>
-                                </tr>
-
-                            </tbody>
-
-                        </table>
-                    </div>
-
-                    <h3>작성한리뷰 n건</h3>
-
-                    <div class="content_tb">
-                        <table>
-                            <colgroup>
-                                <col style="width:15%">
-                                <col style="width:14%">
-                                <col style="width:14%">
-                                <col>
-                            </colgroup>
-                            <thead>
-                                <tr class="th_box">
-                                    <th scope="col" style="">주문상품</th>
-                                    <th scope="col"><span class="blind">주문번호</span></th>
-                                    <th scope="col" style="">작성일</th>
-                                    <th scope="col"><span class="blind">수정/삭제</span></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="td_box">
-                                        <p style=""><span style="">작성한 리뷰가 없습니다.</span></p>
-                                    </td>
-                                </tr>
-
-                            </tbody>
-
-                        </table>
-                    </div>
-                </div>
-
-
+            <ul class="list-group list-group-flush" v-if="sideitem1 ==1">
+              <li class="list-group-item side_item">주문 관리</i></li>
+              <li class="list-group-item side_item">배송 관리</li>              
+            </ul>
+          </div>
+          <div class="card">
+            <div class="card-header hd1"  @click="ONOFF2">
+              활동관리
+              <button v-if="item2bt==0" style="margin-left :85px">
+                <i class="fas fa-chevron-up"></i>
+              </button>
+              <button v-if="item2bt==1" style="margin-left :85px">
+                <i class="fas fa-chevron-down"></i>
+              </button>
             </div>
 
+            <ul class="list-group list-group-flush" v-if="sideitem2 ==1">
+              <li class="list-group-item side_item">리뷰 관리</li>
+              <li class="list-group-item side_item">QnA 관리</li>          
+            </ul>
+          </div>
+          <div class="card">
+            <div class="card-header hd1"  @click="ONOFF3">
+              정보관리
+              <button v-if="item3bt==0" style="margin-left :85px">
+                <i class="fas fa-chevron-up"></i>
+              </button>
+              <button v-if="item3bt==1" style="margin-left :85px">
+                <i class="fas fa-chevron-down"></i>
+              </button>
+            </div>
+
+            <ul class="list-group list-group-flush" v-if="sideitem3 ==1">
+              <li class="list-group-item side_item">회원정보 변경</li>
+              <li class="list-group-item side_item">비밀번호 변경</li>          
+            </ul>
+          </div>
         </div>
 
+        <div class="content_body">
+          <h2 class="body_header">리뷰 관리</h2>
+          <img src="" alt="img" style="width: 777px; height: 62px" />
 
+          <div class="body_content">
+            <h3>미등록리뷰 n건</h3>
+
+            <div class="content_tb">
+              <table>
+                <colgroup>
+                  <col style="width: 14%" />
+                  <col />
+                  <col style="width: 20%" />
+                  <col style="width: 17%" />
+                </colgroup>
+                <thead>
+                  <tr class="th_box">
+                    <th scope="col" style="">주문번호</th>
+                    <th scope="col" style="">주문상품</th>
+                    <th scope="col" style="">
+                      <span class="blind">리뷰 혜택</span>
+                    </th>
+                    <th scope="col" style="">
+                      <span class="blind">리뷰 쓰기</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="td_box">
+                      <p style="">
+                        <span style="">해당 기간에 구매내역이 없습니다.</span>
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <h3>작성한리뷰 n건</h3>
+
+            <div class="content_tb">
+              <table>
+                <colgroup>
+                  <col style="width: 15%" />
+                  <col style="width: 14%" />
+                  <col style="width: 14%" />
+                  <col />
+                </colgroup>
+                <thead>
+                  <tr class="th_box">
+                    <th scope="col" style="">주문상품</th>
+                    <th scope="col"><span class="blind">주문번호</span></th>
+                    <th scope="col" style="">작성일</th>
+                    <th scope="col"><span class="blind">수정/삭제</span></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="td_box">
+                      <p style="">
+                        <span style="">작성한 리뷰가 없습니다.</span>
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
-</body>
-
+  </body>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            sideitem1 :1,
+            sideitem2 :1,
+            sideitem3 :1,
+            item1bt : 1,
+            item2bt : 1,
+            item3bt : 1,
+            
+        }
+        
+    },
+      methods: {
+      
+      ONOFF1() {
+        if (this.sideitem1 == 0) {
+        this.sideitem1 = 1;
+        this.item1bt =1;
+        } else if (this.sideitem1 == 1) {
+        this.sideitem1 = 0;
+        this.item1bt =0;
+        }
+      },
+      ONOFF2() {
+        if (this.sideitem2 == 0) {
+        this.sideitem2 = 1;
+        this.item2bt =1;
+        } else if (this.sideitem2 == 1) {
+        this.sideitem2 = 0;
+        this.item2bt =0;
+        }
 
-}
+      },
+      ONOFF3() {
+        if (this.sideitem3 == 0) {
+        this.sideitem3 = 1;
+        this.item3bt =1;
+        } else if (this.sideitem3 == 1) {
+        this.sideitem3 = 0;
+        this.item3bt =0;
+        }
+      },
+
+    },
+};
+
+
 </script>
 
 <style>
+.body_review{
+  height: 1000px;
+
+}
 /* header setting */
+
 .container_my {
-    /* background-color: aqua; */
-    
-    margin-top: 30px;
-    margin-left: auto;
-    margin-right: auto;
-    border-right: 1px solid #d1dadd;
-    border-left: 1px solid #d1dadd;
-    width: 1020px;
-    height: fit-content;
+  /* background-color: aqua; */
+
+
+  margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  border-right: 1px solid #d1dadd;
+  border-left: 1px solid #d1dadd;
+  width: 1020px;
+  height: 1000px;
+  /* fit content 사용시 풋터가 올라오는 에러 발생  height 고정값 주기*/
 }
 
 .header_my {
-    background-color: #F3FAFE;
-    border-bottom: 3px solid #203a4d;
-    width: 1018px;
-    height: 94px;
-    margin: 20px 0px 20px 0px;
+  background-color: #f3fafe;
+  border-bottom: 3px solid #203a4d;
+  width: 1018px;
+  height: 94px;
+  margin: 20px 0px 20px 0px;
 }
-.header_tag{
-    /* background-color: aquamarine; */
-    height: 37px;
-    width: 139px;
-    float: left;
-    margin: 8px 0px 8px 0px;
-    padding: 32px 15px 9px 18px;
+.header_tag {
+  /* background-color: aquamarine; */
+  height: 37px;
+  width: 139px;
+  float: left;
+  margin: 8px 0px 8px 0px;
+  padding: 32px 15px 9px 18px;
 }
-.user_name{
-    height: 17.5px;
-    width: 53.5px;
-    font-size: 12px;
-
+.user_name {
+  height: 17.5px;
+  width: 53.5px;
+  font-size: 12px;
 }
-.ssg_img{
-    height: 37px;
-    width: 139px;
+.ssg_img {
+  height: 37px;
+  width: 139px;
 }
-
 
 /* content area */
 /* content_side setting  + content_body setting*/
 
-.content_my{
-    width: 1018px;
-    height: 1000px;
-    /* border : solid 3px red; */
+.content_my {
+  width: 1018px;
+  height: fit-content;
+  /* border : solid 3px red; */
 }
 
-.content_side{
-    /* background-color: aqua; */
-    float: left;
-    width: 200px;
-    height: 600px;
-    
+.content_side {
+  /* background-color: aqua; */
+  float: left;
+  width: 200px;
+  height: 600px;
 }
 
-.btn_size{
-    width: 200px;
-    height: 60px;
+.btn_size {
+  width: 200px;
+  height: 60px;
 }
 
+/* ------------------------side bar start-------------------------------- */
+.hd1{
+  cursor: pointer;
+}
+
+.side_item {
+  background-color: #f3fafe;
+}
+
+
+.side_item:hover {
+  background-color: cadetblue;
+  display: inline-block
+}
+.side_item:active {
+  background-color: lightskyblue;
+}
 
 /* ------------------------side bar end-------------------------------- */
 
-
 /* ------------------------content_my body start-------------------------------- */
-.content_body{
-    /* background-color: lightgray; */
-    float: left;
-    margin-left: 35px;
-    width: 777px;
-    height: 600px;
+.content_body {
+  /* background-color: lightgray; */
+  float: left;
+  margin-left: 35px;
+  width: 777px;
+  height: 600px;
 }
 
-.body_header{
-    height: fit-content;
-    width: 780px;
-    padding: 15px 0px 13px 10px;
-    border-bottom: 1px solid #203a4d;
-    line-height: 30px;
+.body_header {
+  height: fit-content;
+  width: 780px;
+  padding: 15px 0px 13px 10px;
+  border-bottom: 1px solid #203a4d;
+  line-height: 30px;
 }
-.body_content{
-    /* background-color: aquamarine; */
-    margin-top: 30px;
-    height: 207px;
-    width: 777px;
-}
-
-.content_tb{
-    /* background-color: brown; */
+.body_content {
+  /* background-color: aquamarine; */
+  margin-top: 30px;
+  height: 207px;
+  width: 777px;
 }
 
-.blind{
-    display: none;
+.content_tb {
+  /* background-color: brown; */
 }
 
-h2{
-    margin : 0px;
-    padding: 0px;
-    display: block;
-    font-size: 26px;
-    
+.blind {
+  display: none;
 }
 
-h3{
-    font-size: 20px;
-    margin-top: 20px;
+h2 {
+  margin: 0px;
+  padding: 0px;
+  display: block;
+  font-size: 26px;
 }
 
-thead{
-    /* background: red; */
-    height: 18px;
-    width: 80px;
-    padding: 15px;
-    border-bottom: 1px solid #ededed;
-    color: #222;
-    font-size: 12px;
+h3 {
+  font-size: 20px;
+  margin-top: 20px;
 }
 
-.th_box{
-    background-color: lightgray;
-    width: 777px;
-    height: 40px;
-    border-top: 1px solid #203a4d;
-    border-bottom: 1px solid #203a4d;
-    margin-top: 10px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
+thead {
+  /* background: red; */
+  height: 18px;
+  width: 80px;
+  padding: 15px;
+  border-bottom: 1px solid #ededed;
+  color: #222;
+  font-size: 12px;
 }
 
-.td_box{
-    width: 777px;
-    height: 100px;
-    border-bottom: 1px solid #203a4d;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.th_box {
+  background-color: lightgray;
+  width: 777px;
+  height: 40px;
+  border-top: 1px solid #203a4d;
+  border-bottom: 1px solid #203a4d;
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
 
+.td_box {
+  width: 777px;
+  height: 100px;
+  border-bottom: 1px solid #203a4d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 /* ------------------------content_my body end-------------------------------- */
-
-
-
 </style>
